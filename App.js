226fb31fa9db8d815589a8d4458d4bf572fbf7df
jaslioin <15674058+jaslioin@users.noreply.github.com>
2021-05-08 01:30:32 +0800

    Tidy up

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
-import { NavigationContainer ,} from '@react-navigation/native';
 import { createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Dimensions, StyleSheet, View } from 'react-native';
 import Home from './src/views/Home';
 import { Z_INDEX_CONFIG } from './src/constants';
 import { useState } from 'react';
-import TestGround from './src/views/TestGround';
 import { Button } from 'react-native-elements';
-const Tab = createBottomTabNavigator();
 const ZOOM_LEVELS=[0.5,0.75,1,1.25,1.5]
 export default function App() {  
   const [zoomLevel,setZoomLevel]=useState(0)
@@ -33,65 +30,6 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Home zoomLevel={ZOOM_LEVELS[zoomLevel]}/>
-      {/* <TestGround/> */}
-        {/* <NavigationContainer>
-        <Tab.Navigator
-							// initialRouteName={BOTTOM_TAB_NAME.MY_PROFILE}
-							screenOptions={
-                ({ route }) => ({
-                  // tabBarIcon: ({ focused, color, size }) => {
-                  //   if (
-                  //     route.name === BOTTOM_TAB_NAME.AGENT_LIST ||
-                  //     route.name === BOTTOM_TAB_NAME.CLIENT_LIST
-                  //   ) {
-                  //     return (
-                  //       <Image
-                  //         source={require("./src/asset/icon/people_48x48.png")}
-                  //       />
-                  //     );
-                  //   } else if (route.name === BOTTOM_TAB_NAME.CHAT) {
-                  //     return (
-                  //       <Image
-                  //         source={require("./src/asset/icon/chat_48x48.png")}
-                  //       />
-                  //     );
-                  //   } else if (route.name === BOTTOM_TAB_NAME.MY_PROFILE) {
-                  //     return (
-                  //       <Image
-                  //         source={require("./src/asset/icon/profile_48x48.png")}
-                  //       />
-                  //     );
-                  //   }
-                  // },
-                  unmountOnBlur:false
-                    // Platform.OS === "android" &&
-                    // (route.name === BOTTOM_TAB_NAME.AGENT_LIST ||
-                    // 	route.name === BOTTOM_TAB_NAME.CLIENT_LIST),
-                })
-              }
-							tabBarOptions={{
-								// activeTintColor: color.main,
-								// inactiveTintColor: color.main,
-								// showLabel: false,
-								// activeBackgroundColor: color.main,
-								// inactiveBackgroundColor: color.main,
-								// style: {
-								// 	display: state.isBottomTabDisplay ? "flex" : "none",
-								// },
-							}}
-						>
-							<Tab.Screen
-								name={"HI"}
-								component={Home}
-								screenOptions={{}}
-							/>
-							<Tab.Screen
-								name={"Ho"}
-								component={Home}
-								screenOptions={{}}
-							/>
-						</Tab.Navigator>
-        </NavigationContainer> */}
         <View style={{position:'absolute',zIndex:Z_INDEX_CONFIG.HIGHEST,backgroundColor:'orange',height:100,width:100 ,top:Dimensions.get('screen').height/2,left:0,
           justifyContent:'center',
           alignItems:'center'
